8c0622a2fa84627463d0299f708860fe9e444ef9
Julia Camacho <49563129+juliacamacho@users.noreply.github.com>
2022-06-02 12:22:17 -0700

    add all 4 tabs

---
+++ js/router.js
@@ -0,0 +1,35 @@
+window.onload = function() {
+
+const route = (event) => {
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+    handleLocation();
+};
+
+const routes = {
+    "/": "../home.html",
+    // "/motivation": "../migrant-motivation.html",
+    // "/cost": "../migrant-cost.html",
+    "/about": "../about.html"
+};
+
+const handleLocation = async () => {
+    const path = window.location.pathname;
+    // console.log("FULL PATH:", full_path)
+    // const path = full_path.split('/')[1]
+    console.log("PATH:", path)
+    const route = routes[path] || routes[404];
+    // window.location.href = route;
+    const html = await fetch(route).then((data) => data.text());
+    document.getElementById("page-content").innerHTML = html;
+};
+
+window.onpopstate = handleLocation;
+window.route = route;
+
+handleLocation();
+
+}
+
+//todo: instead use for when migrant pages load -> replace data
